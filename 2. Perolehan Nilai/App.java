63242d9500c9b15433dbe3efe6ecf89ee0e64d3f
Gilbert Gultom <ifgilbert24@gmail.com>
2026-09-20 17:09:38 +0700

    update

--- 2. Perolehan Nilai/App.java
+++ 2. Perolehan Nilai/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Locale;
 
-public class App1 {
+public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         if (!sc.hasNextInt()) {
--- 4. Paling Ter/App.java
+++ 4. Paling Ter/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class App3 {
+public class App {
     public static void main(String[] args) {
         ArrayList<Integer> data = bacaData();
         if (data.isEmpty()) return;
